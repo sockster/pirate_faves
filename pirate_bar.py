cf084aaf16635967e9bc19deffaf14bf7c1de761
Sailor Socks <socksster@gmail.com>
2015-06-08 20:38:06 -0400

    break down add to empty dictionary

--- pirate_bar.py
+++ pirate_bar.py
@@ -16,13 +16,10 @@ while count <= 5
 repeat up to count =ing 5 and then cease looping
 """
 
-questions = {
-	"strong": "Do ye like yer drinks strong?",
-	"salty": "Do ye like it with a salty tang?",
-	"bitter": "Are ye a lubber who likes it bitter?",
-	"sweet": "Would ye like a bit of sweetness with yer poison?",
-	"fruity": "Are ye one for a fruity finish?"
-}
+
+import random
+
+
 
 ingredients = {
 	"strong": ["glug of rum", "slug of whisky", "splash of gin"],
@@ -33,30 +30,69 @@ ingredients = {
 }
 
 
-def drinks():
-	count = 0			# add to count for each question asked
-	drink_desc = []		# append to store desc name for each y ans
-	drink_ingr = []		# append to store random ingr for each y ans
-	
-	while count <= 5:
-		print questions["strong"],
-		print ("Y or N?")
-		strong_ans = raw_input()
-		if strong_ans == "Y" or strong_ans == "y":
-			print "Aye!"
-		elif strong_ans == "N" or strong_ans == "n":
-			print "Arrgh!"
-		else:
-			print "Ye son of a biscuit eater! Be ye tryin' to scuttle me rum makin'?"
-		
-	
+questions = {
+	"strong": "Do ye like yer drinks strong?",
+	"salty": "Do ye like it with a salty tang?",
+   	"bitter": "Are ye a lubber who likes it bitter?",
+   	"sweet": "Would ye like a bit of sweetness with yer poison?",
+   	"fruity": "Are ye one for a fruity finish?"
+}
 
+
+"""    just testing - all of these work
+print questions["strong"]
+print drink_desc
+print drink_ingr
+ingredients["tart"] = "add lime"		<--- adds "tart": ["add lime"] to ingredients dictionary
+
+"""	
+
+drink_desc = {}
+		# append to store desc name for each y ans
+drink_ingr = {}
+		# append to store random ingr for each y ans
+
+
+def drinks():
+	print questions["strong"]
+	print ("Y or N?")
+	strong_ans = raw_input()
+	if strong_ans == "Y" or strong_ans == "y":
+		print "Aye!"
+		drink_desc = ingredients["strong"]
+	elif strong_ans == "N" or strong_ans == "n":
+		print "Arrgh!"
+	else:
+		print "Ye son of a biscuit eater! Be ye tryin' to scuttle me rum makin'?"
 
 
 if __name__ == '__main__':
+
 	drinks()
+	print ingredients["strong"]
+	print drink_desc.items()
+
 print ""
 print ""
 
+
+
+
+
+
+"""
+def drinks():
+	count = 0			# add to count for each question asked
+	
+	while count <= 5:
+		questions()
+		count += 1
+
+
+
+
+
+
+"""
 
 
